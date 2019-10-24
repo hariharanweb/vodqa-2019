cb2c6c0c6404e25beb34b3e0d94658877292f3a4
shridhar kalagi <shridharkalagi@gmail.com>
2019-10-24 15:02:05 +0530

    refactor RTL test

--- src/integ-tests/integRTL.spec.js
+++ src/integ-tests/integRTL.spec.js
@@ -1,25 +1,11 @@
-import React from 'react';
-import { render, fireEvent } from '@testing-library/react';
-
-import vodQAReducer from '../reducers';
-import { createStore } from 'redux';
-import { AppRouter } from '../App';
-import { Provider } from 'react-redux';
-import {
-    createHistory,
-    createMemorySource,
-    LocationProvider,
-} from '@reach/router';
+import { fireEvent } from '@testing-library/react';
+import { renderWithRouter, customfireEvent } from './integRTLHeplers';
 
 describe('Sample test', () => {
     it('Validate the login Button Text', async () => {
         const {
             history: { navigate },
-            getByLabelText, getByText } = renderWithRouter(
-            <Provider store={createReduxStore()}>
-                <AppRouter />
-            </Provider>
-        );
+            getByLabelText, getByText } = renderWithRouter();
 
         expect(getByText('Welcome to VodQA')).toBeDefined();
         fireEvent.change(getByLabelText('Username:'), { target: { value: 'chuck' } });
@@ -32,32 +18,3 @@ describe('Sample test', () => {
     });
 });
 
-function renderWithRouter(
-    ui,
-    { route = '/', history = createHistory(createMemorySource(route)) } = {}
-) {
-    return {
-        ...render(<LocationProvider history={history}>{ui}</LocationProvider>),
-        // adding `history` to the returned utilities to allow us
-        // to reference it in our tests (just try to avoid using
-        // this to test implementation details).
-        history,
-    };
-}
-
-
-const initialState = {};
-export const createReduxStore = (overiddenState = {}) => {
-    const allReducers = Object.assign(vodQAReducer);
-    const actualState = Object.assign({}, initialState, overiddenState);
-
-    return createStore(allReducers, actualState);
-};
-
-
-// component.find('#username').simulate('change', {target: {value: 'user'}});
-//         component.find('#password').simulate('change', {target: {value: 'password'}});
-//         component.find('#submit').simulate('click');
-
-//         await waitForElementToAppear(component,'#header');
-//         expect(component.find('#header').text()).toContain('2019');
